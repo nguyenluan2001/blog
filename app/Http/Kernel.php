1d650b7c983d30f4135cff59338dbfe1aee9b963
nguyenluan2001 <ntluan0301@gmail.com>
2020-12-30 17:26:53 +0700

    deploy 10

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,6 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'AllowCors'=>\App\Http\Middleware\AllowCors::class,
-        'PreFlightRespone'=>\App\Http\Middleware\PreFlightResponse::class
+        'PreFlightResponse'=>\App\Http\Middleware\PreFlightResponse::class
     ];
 }
