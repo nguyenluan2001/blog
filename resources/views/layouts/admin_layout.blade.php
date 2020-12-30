d6bae52f6b26de3d1e9bccea3c9eccafed9fc989
nguyenluan2001 <ntluan0301@gmail.com>
2020-12-30 16:40:53 +0700

    deploy 5

--- resources/views/layouts/admin_layout.blade.php
+++ resources/views/layouts/admin_layout.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
     <title>Document</title>
     <link rel="stylesheet" href="{{url('resources/css/admin/style.css')}}">
     <base href="http://localhost:8080/TheCodeholic/">
